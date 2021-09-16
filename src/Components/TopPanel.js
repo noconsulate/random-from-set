1fe4ec78cc4d9de20a003c279f89fadf5c3b7b9f
Janssen Kuhn <noconsulate@gmail.com>
2021-09-16 11:30:59 -0600

    envs not working

--- src/Components/TopPanel.js
+++ src/Components/TopPanel.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { supabase } from "../Data/supabaseClient";
 import "../Styles/Components/TopPanel.scss";
+
+const handleNew = async () => {
+  const { data, error } = await supabase.from("instances").insert();
+
+  console.log(data, error);
+};
 
 const TopPanel = (props) => {
   return (
@@ -20,6 +27,10 @@ const TopPanel = (props) => {
   );
 };
 
-const New = () => <button className="button left">New</button>;
+const New = () => (
+  <button onClick={() => handleNew()} className="button left">
+    New
+  </button>
+);
 
 export default TopPanel;
---
+++ src/Data/supabaseClient.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+console.log(process.env);
+
+const url = process.env.REACT_APP_SUPABASE_URL;
+const anonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(url, anonKey);
