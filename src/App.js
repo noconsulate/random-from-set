6609fbac5193096cd0f4ea69e084355ee907ab66
Janssen Kuhn <noconsulate@gmail.com>
2021-09-16 12:14:37 -0600

    set route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Switch, Route } from "react-router-dom";
 
 import TopPanel from "./Components/TopPanel";
 import Root from "./Components/Root";
+import Set from "./Components/Set";
 
 const App = () => {
   return (
     <div>
       <TopPanel />
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Root />
+        </Route>
+        <Route path="/set">
+          <Set />
         </Route>
       </Switch>
     </div>
---
+++ src/Components/Set.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Set = () => <>set</>;
+
+export default Set;
