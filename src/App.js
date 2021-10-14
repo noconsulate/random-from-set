35ce560213658b6829d6a6504484cbc57bd61f74
Janssen Kuhn <noconsulate@gmail.com>
2021-10-14 10:30:56 -0600

    SW lifecycle listeners

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { ServiceWorkerUpdateListener } from "./ServiceWorkerUpdateListener.js";
 
 import TopPanel from "./Components/TopPanel";
 import Root from "./Components/Root";
 import Set from "./Components/Set";
 
 const App = () => {
+  const listener = new ServiceWorkerUpdateListener();
+  listener.onupdateinstalling = (installingevent) => {
+    console.log("SW installed");
+  };
+  listener.onupdatewaiting = (waitingevent) => {
+    console.log("new update waiting");
+  };
+
+  navigator.serviceWorker
+    .getRegistration()
+    .then((reg) => listener.addRegistration(reg));
+
   return (
     <div className="topParent">
+      <h1>fgf</h1>
       <TopPanel />
       <Switch>
         <Route exact path="/">
